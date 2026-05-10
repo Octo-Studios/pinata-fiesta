ccb7882ecfe2f703e817b2e95eb4540db1066a81
binary <catboybinary@disroot.org>
2026-05-10 22:23:26 +0300

    default template

---
+++ common/src/main/java/it/hurts/shatterbyte/pinatafiesta/Constants.java
@@ -0,0 +1,10 @@
+package it.hurts.shatterbyte.pinatafiesta;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "pinatafiesta";
+	public static final String MOD_NAME = "Pinata Fiesta";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
